b0f8989b35bdba267432bea6a6317b7a65a5b2b6
Christian Tietze <christian.tietze@gmail.com>
2017-05-17 08:21:24 +0200

    snap still image after 4s

--- WebcamSnap/AppDelegate.swift
+++ WebcamSnap/AppDelegate.swift
@@ -1,27 +1,112 @@
-//
-//  AppDelegate.swift
-//  WebcamSnap
-//
-//  Created by Christian Tietze on 17/05/17.
-//  Copyright © 2017 Christian Tietze. All rights reserved.
-//
+//  Copyright © 2017 Christian Tietze. All rights reserved. Distributed under the MIT License.
 
 import Cocoa
+import AVFoundation
+
+extension String: Error {}
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    // MARK: -
+
+    @IBOutlet weak var resultImageView: NSImageView!
+
+    class Webcam {
+
+        let session: AVCaptureSession
+        let stillImageOutput: AVCaptureStillImageOutput
+
+        init() throws {
+
+            let session = AVCaptureSession()
+            session.sessionPreset = AVCaptureSessionPresetMedium
+
+            let stillOutput = AVCaptureStillImageOutput()
+            stillOutput.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+            session.addOutput(stillOutput)
+
+            let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+            let input = try AVCaptureDeviceInput(device: device)
+            guard session.canAddInput(input) else { throw "Cannot add input" }
+            session.addInput(input)
+
+            self.session = session
+            self.stillImageOutput = stillOutput
+        }
+
+        func start() {
+            session.startRunning()
+        }
+
+        func stop() {
+            session.stopRunning()
+        }
+
+        deinit {
+            if session.isRunning { stop() }
+        }
+
+        func captureImage(result: @escaping (NSImage?, Error?) -> Void) {
+
+            let connection = stillImageOutput.connection(withMediaType: AVMediaTypeVideo)
+            stillImageOutput.captureStillImageAsynchronously(from: connection) { (imageBuffer: CMSampleBuffer?, error: Error?) in
+
+                guard let data = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageBuffer) else {
+                    result(nil, "JPEG data could not be created")
+                    return
+                }
+
+                guard let image = NSImage(data: data) else {
+                    result(nil, "NSImage conversion failed")
+                    return
+                }
+
+                result(image, nil)
+            }
+        }
     }
 
+    var session: Webcam?
 
+    @IBAction func newSnap(_ sender: Any) {
+
+        do {
+            self.session = try Webcam()
+        } catch {
+            self.session?.stop()
+            return
+        }
+
+        self.session?.start()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(4)) {
+
+            self.session?.captureImage(result: { (image, error) in
+
+                defer { self.session?.stop() }
+
+                if let error = error {
+                    print("\(error)")
+                    return
+                }
+
+                guard let image = image else {
+                    print("no image")
+                    return
+                }
+
+                DispatchQueue.main.async {
+                    self.resultImageView.image = image
+                }
+            })
+        }
+    }
 }
 
