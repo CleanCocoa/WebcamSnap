f4384959f38308fbfac8e6428a47d3956fda0bc1
Christian Tietze <christian.tietze@gmail.com>
2017-05-17 10:50:15 +0200

    add tools to drive the IKImageView

--- WebcamSnap/AppDelegate.swift
+++ WebcamSnap/AppDelegate.swift
@@ -1,6 +1,7 @@
 //  Copyright © 2017 Christian Tietze. All rights reserved. Distributed under the MIT License.
 
 import Cocoa
+import Quartz
 
 // Make Strings throwable
 extension String: Error {}
@@ -12,13 +13,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
 
+        self.currentTool = .pan
     }
 
     // MARK: -
 
-    @IBOutlet weak var resultImageView: NSImageView!
-    @IBOutlet weak var previewView: NSView!
     lazy var snapPicture: SnapPicture = SnapPicture()
+    @IBOutlet weak var resultImageView: IKImageView!
+    @IBOutlet weak var placeholderLabel: NSTextField!
+    @IBOutlet weak var toolsSegmentedControl: NSSegmentedControl!
 
     @IBAction func newSnap(_ sender: Any) {
 
@@ -26,9 +29,53 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             switch result {
             case .cancel: break
             case .error(let error): print("error taking picture: \(error)")
-            case .picture(let image): self.resultImageView.image = image
+            case .picture(let image):
+                self.placeholderLabel.isHidden = true
+                self.resultImageView.setImage(
+                    image.cgImage(forProposedRect: nil, context: nil, hints: nil),
+                    imageProperties: [:])
             }
         }
+    }
+
+    enum Tool: Int {
+        case pan = 0
+        case crop = 1
+        case rotate = 2
+
+        var toolMode: String {
+            switch self {
+            case .pan: return IKToolModeMove
+            case .crop: return IKToolModeCrop
+            case .rotate: return IKToolModeRotate
+            }
+        }
+    }
+
+    var currentTool: Tool! {
+        didSet {
+            resultImageView.currentToolMode = currentTool.toolMode
+            toolsSegmentedControl.selectedSegment = currentTool.rawValue
+        }
+    }
+
+    @IBAction func switchTool(_ sender: Any) {
+
+        func rawToolValue(control: Any) -> Int? {
+
+            switch control {
+            case let segmentedControl as NSSegmentedControl:
+                return segmentedControl.selectedSegment
+
+            default: return nil
+            }
+        }
+
+        guard let rawValue = rawToolValue(control: sender),
+            let tool = Tool(rawValue: rawValue)
+            else { assertionFailure("Couldn't figure out tool."); return }
+
+        resultImageView.currentToolMode = tool.toolMode
     }
 }
 
