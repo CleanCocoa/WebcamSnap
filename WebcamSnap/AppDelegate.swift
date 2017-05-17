473212db9d62a2ff4672c6535bd74b5fd55e5ae5
Christian Tietze <christian.tietze@gmail.com>
2017-05-17 07:30:40 +0200

    Initial Commit

---
+++ WebcamSnap/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WebcamSnap
+//
+//  Created by Christian Tietze on 17/05/17.
+//  Copyright © 2017 Christian Tietze. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
